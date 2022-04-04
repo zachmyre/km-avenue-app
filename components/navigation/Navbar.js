ed9390f745c53f903bfbc9c8b83cb0b77f558183
Zachary Myre <zmyre@securitybrandsinc.com>
2022-04-04 16:28:20 -0500

    Update Navbar.js

--- components/navigation/Navbar.js
+++ components/navigation/Navbar.js
@@ -1,10 +1,69 @@
+import { useState } from 'react';
 import Image from 'next/image'
+import Link from 'next/link'
 import vercel from '../../public/vercel.svg'
 
 export default function Navbar(){
-    return (
-        <nav className="bg-red-500 border-black-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-            <Image src={vercel} alt="Vercel Logo" width={72} height={72} />
-        </nav>
-    )
+      const [navbarOpen, setNavbarOpen] = useState(false);
+      return (
+        <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-300 mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full items-center relative flex justify-between lg:w-auto lg:static lg:block lg:justify-center">
+              <Link href="/">
+                <a
+                className="text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white">
+                KM Avenue
+                </a>
+            </Link>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <Image src={vercel} width={75} height={50} />
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="lg:flex lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <Link href='/'>
+                    <a
+                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                    >
+                        <span className="ml-2">Home</span>
+                    </a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href='/products'>
+                    <a
+                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                    >
+                        <span className="ml-2">Products</span>
+                    </a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href='/contact'>
+                    <a
+                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                    >
+                        <span className="ml-2">Contact</span>
+                    </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div class="hidden lg:block">
+              <Image src={vercel} width={75} height={50} />
+          </div>
+        </div>
+      </nav>
+      );
 }
