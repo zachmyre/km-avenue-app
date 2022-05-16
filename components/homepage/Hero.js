0fd38f1f9312f8211e5b0f1aca5cbab4581d6d0c
Zachary Myre <zmyre@securitybrandsinc.com>
2022-05-16 15:29:43 -0500

    Update Hero.js

--- components/homepage/Hero.js
+++ components/homepage/Hero.js
@@ -43,7 +43,7 @@ export default function Hero() {
       <div className="relative bg-white overflow-hidden">
       <main className="mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
         <div className="sm:text-center lg:text-left w-full lg:w-1/2">
-          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <h1 className="mt-3 md:mt-0 text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
             <span className="block xl:inline">Fits for you, </span>
             <span className="block text-kandy-pink xl:inline">a boutique for everyone.</span>
           </h1>
